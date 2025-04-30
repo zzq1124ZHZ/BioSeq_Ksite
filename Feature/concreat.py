6a9732662b61edd13b21b2d396b2f57868b15eef
zzq1124ZHZ <147896362+zzq1124ZHZ@users.noreply.github.com>
2025-04-30 15:41:37 +0800

    Update concreat.py

--- Feature/concreat.py
+++ Feature/concreat.py
@@ -5,6 +5,6 @@ import numpy as np
 meiler_feature= pd.read_csv(r"meiler.csv",header=None)
 pt5_feature= pd.read_csv(r"pt5.csv",header=None)
 concreat_feature_neg=np.concatenate([pt5_feature,meiler_feature], axis=1)
-csv_file_path= r'feature.csv'#
+csv_file_path= r'feature.csv'
 np.savetxt(csv_file_path,concreat_feature_neg, delimiter=',')
-print(f"CSV文件已保存到: {csv_file_path}")
+
