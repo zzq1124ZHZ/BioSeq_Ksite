1cf90ba6a25e7b7c348af9c382fb09bf41f0ae6d
zzq1124ZHZ <147896362+zzq1124ZHZ@users.noreply.github.com>
2025-05-09 21:22:47 +0800

    Delete Feature/divid-test-train/divid_text.py

--- Feature/divid-test-train/divid_text.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-df = pd.read_csv( r"concreat_neg_feature.csv",header=None)
-# train and test
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=3047)
-test_df.to_csv(r'divid_test_train\test_dataset.csv', index=False)
-train_df.to_csv(r'divid_test_train\train_dataset.csv', index=False)
